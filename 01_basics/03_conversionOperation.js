76f3509bb3737b340a16b163b780e1ecd939f389
Arpit Ganesh Ghorpade <147497068+arpitghorpade@users.noreply.github.com>
2024-07-23 15:10:27 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = "33";   
+// let score = null;
+// let score = "undefine";
+// let score = true;
+// let score = "arpit";
+
+// console.log(typeof score);
+// console.log(typeof(score));   
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+// let isLoggedIn = 1;
+// let isLoggedIn = 0; 
+// let isLoggedIn = "";
+// let isLoggedIn = "arpit";
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/* Note
+ 1 => true; 0 => false
+"" => false
+"arpit" => true */
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
