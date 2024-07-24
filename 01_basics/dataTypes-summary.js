d87d773d84f22a6d3412b61454e267d85c2ec9e2
Arpit Ganesh Ghorpade <147497068+arpitghorpade@users.noreply.github.com>
2024-07-24 15:45:12 +0000

    dataType-summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,34 @@
+// Two Types of dataTypes in js
+// Primitive and non primitive
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+//1st - DataType => primitive data type has 7 types
+// Number, String, Boolean, Undefined, Null, Bigint, Symbol
+
+// 2nd - DataType => Reference (non primitive)
+// Objects, Array, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "arpit",
+    age: 20,
+}
+
+const myFunction = function() {
+    console.log("hello World");
+}
+console.log(typeof myObj);
+
+
+// link => https://262.ecma-international.org/5.1/#sec-11.4.3
