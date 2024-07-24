f7c0a2f35c6fe6d4f743cb9bf82491aea85298fe
Arpit Ganesh Ghorpade <147497068+arpitghorpade@users.noreply.github.com>
2024-07-24 17:52:13 +0000

    stack and heap memory

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -32,3 +32,28 @@ console.log(typeof myObj);
 
 
 // link => https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ***************************************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myFullName = "arpitghorpade";
+
+let anotherName = myFullName;
+anotherName = "dynanesh";
+
+console.log(myFullName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "arpit@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
