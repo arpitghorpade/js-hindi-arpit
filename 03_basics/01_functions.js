4e4dd0677b420b2523d9f5d906a7bef8ab9da1ff
Arpit Ganesh Ghorpade <147497068+arpitghorpade@users.noreply.github.com>
2024-08-10 19:28:40 +0000

    functions with objects and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,14 +22,44 @@ const result = addTwoNumber(4, 4);
 // console.log("result:", result);
  
 
-function loginUserMessage(username = "arpit") {
-    if(!username){
-        console.log("Please enter a username");
-        return
+// function loginUserMessage(username = "arpit") {
+//     if(!username){
+//         console.log("Please enter a username");
+//         return
         
-    }
-    return `${username} just logged in`;
-}
+//     }
+//     return `${username} just logged in`;
+// }
 
 // console.log(loginUserMessage("Arpit"));
-console.log(loginUserMessage("arpit"));
+// console.log(loginUserMessage("arpit"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "arpit",
+    price: 100
+} 
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "arpit",
+    price: 300
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
