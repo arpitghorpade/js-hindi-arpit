d7650a64405a62d86b1694c2b0c1635bc83d22cd
Arpit Ganesh Ghorpade <147497068+arpitghorpade@users.noreply.github.com>
2024-08-22 15:02:33 +0000

    some practice changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,15 +53,15 @@ let str2 = "arpit";
 let str3 = str1 + str2;
 // console.log(str3); 
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
-// console.log( (3 + 4) * 5 % 3);
+console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);
+console.log(+"");
 
 let num1, num2, num3
 
